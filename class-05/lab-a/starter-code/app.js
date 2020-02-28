95abe6feeb31de6c4894dad3d814ca07a10d3c16
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-28 15:33:18 -0600

    solved problem 3 with passing test

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -52,12 +52,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { 
+    var sumThree = sum(sum(a, b)[0], c)[0];
+    var multiplyThree = multiply(multiply(a, b)[0], c)[0];
+    var sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + sumThree + '.';
+    var productMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyThree + '.';
+    return [sumThree, multiplyThree, sumMessage, productMessage]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
